f809fe737da911b5cb1c85ea1977946720dc143c
Michael Dillie <michaeldillie@gmail.com>
2015-09-22 19:12:36 -0500

    added

---
+++ dist/scripts/bundle.js
@@ -0,0 +1,26 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var user = new UserModel();
+var App = Backbone.Router.extend({
+	routes: {
+		'': 'profile',
+		'edit': 'edit'
+	},
+	profile: function profile() {
+		$('.page').hide();
+		$('#profile').show();
+	},
+	edit: function edit() {
+		$('.page').hide();
+		$('#edit').show();
+	}
+});
+
+var app = new App();
+Backbone.history.start();
+
+},{}]},{},[1])
+
+
+//# sourceMappingURL=bundle.js.map
